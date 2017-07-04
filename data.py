fda7c1cef16b08b00ecdd0503c1bff22eb56bd13
chetan reddy <chetanmreddy@outlook.com>
2017-07-04 12:54:00 +0530

    Update data.py

--- data.py
+++ data.py
@@ -8,11 +8,11 @@ Created on Sun Jun 11 11:58:21 2017
 import tweepy
 from tweepy import OAuthHandler
 import json
- 
-consumer_key = 'tXDMxiycRFMEqZUMgF8eiOnKi'
-consumer_secret = 'HFqfmJeErViUQGYEcpH2mXbSnzN1NLKtSaRW0mHind9V0CBvg3'
-access_token = '2229878924-F6wIrxf0U86O6icfXyrDyX4NYAkqAH2oBKg7fUr'
-access_secret = 'f1HrOIUk2X1q7LmVXHnyksnfeL0iLEDJUj1vudqKJYED4'
+# palce your keys in the below fields to acces data. 
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_secret = ''
  
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
