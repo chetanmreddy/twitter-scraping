085b49d82e64f92866030f4479d9bb52ea25da77
chetan reddy <chetanmreddy@outlook.com>
2017-07-04 10:47:38 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+consumer_key = 'tXDMxiycRFMEqZUMgF8eiOnKi'
+consumer_secret = 'HFqfmJeErViUQGYEcpH2mXbSnzN1NLKtSaRW0mHind9V0CBvg3'
+access_token = '2229878924-F6wIrxf0U86O6icfXyrDyX4NYAkqAH2oBKg7fUr'
+access_secret = 'f1HrOIUk2X1q7LmVXHnyksnfeL0iLEDJUj1vudqKJYED4'
